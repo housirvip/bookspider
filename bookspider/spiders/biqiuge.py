5a816bbeb2a3149ebda9d51693a29127fd40de18
housirvip <housirvip@outlook.com>
2019-03-08 09:21:16 +0800

    init project

---
+++ bookspider/spiders/biqiuge.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from scrapy import Selector, Spider, Request, log
+from scrapy.http import Response
+
+from bookspider.items import EssayItem
+
+
+class BiQiuGeSpider(Spider):
+    name = 'biqiuge'
+    allowed_domains = ['www.biqiuge.com']
+    start_urls = ['https://www.biqiuge.com/book/16513/']
+
+    def parse(self, response: Response):
+        a_list = response.xpath("//div[@class='listmain']/dl/dd/a")
+        for a in a_list:
+            uri = a.xpath("@href").get()
+            url = response.urljoin(uri)
+            yield Request(url, callback=self.parse_essay)
+
+    def parse_essay(self, response: Selector):
+        div = response.xpath("//div[@class='content']")
+        title = div.xpath("h1/text()").get()
+        content_list = div.xpath("div[@id='content']/text()").getall()
+        content = ''.join(content_list).strip()
+        self.log(title, level=log.INFO)
+        return EssayItem(title=title, content=content)
